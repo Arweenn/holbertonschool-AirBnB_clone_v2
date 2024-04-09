bfa46c435748cf20826f8d3ef1db1125c63a4e06
Arweenn <arwendumont25@outlook.fr>
2024-04-09 12:08:38 +0200

    task 4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+Script that starts a Flask web app.
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def display_hello():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def display_hbnb():
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_c(text):
+    text = text.replace('_', ' ')
+    return f'C {text}'
+
+
+@app.route('/python', strict_slashes=False, defaults={'text': 'is_cool'})
+@app.route('/python/<text>', strict_slashes=False)
+def display_python(text):
+    text = text.replace('_', ' ')
+    return f'Python {text}'
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def display_number(n):
+    try:
+        return f'{n} is a number'
+    except TypeError:
+        return f'{n} must be an integer'
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
