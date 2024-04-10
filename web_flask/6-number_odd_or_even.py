f28ff41be93c781988c93e423ad50623ee175b08
Arweenn <arwendumont25@outlook.fr>
2024-04-10 15:58:13 +0200

    task 6

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -5,7 +5,7 @@ Script that starts a Flask web app.
 
 from flask import Flask, render_template
 
-app = Flask(__name__, template_folder='./templates')
+app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
