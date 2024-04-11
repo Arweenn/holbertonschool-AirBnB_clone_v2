6d8da13fbfd38ca1e6ca91465c54588c14730db3
Arweenn <arwendumont25@outlook.fr>
2024-04-11 10:36:18 +0200

    task 10

---
+++ web_flask/9-states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Starts a Flask web app."""
+from models import storage
+from flask import render_template, Flask
+
+flk = Flask(__name__)
+
+
+@flk.route("/states", strict_slashes=False)
+def states():
+    states = storage.all("State")
+    return render_template("9-states.html", state=states)
+
+
+@flk.route("/states/<id>", strict_slashes=False)
+def states_id(id):
+    for state in storage.all("State").values():
+        if state.id == id:
+            return render_template("9-states.html", state=state)
+    return render_template("9-states.html")
+
+
+@flk.teardown_appcontext
+def teardown(exc):
+    storage.close()
+
+
+if __name__ == "__main__":
+    flk.run(host='0.0.0.0', port=5000, debug=False)
