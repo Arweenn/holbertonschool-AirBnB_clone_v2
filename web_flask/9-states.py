c41d49a8ddc4f1455b85ea71fcb4d9781e34241e
Arweenn <arwendumont25@outlook.fr>
2024-04-11 10:54:42 +0200

    last tasks

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
-"""Starts a Flask web app."""
+"""
+Script that starts a Flask web app.
+"""
+
 from models import storage
 from flask import render_template, Flask
 
-flk = Flask(__name__)
+app = Flask(__name__)
 
 
-@flk.route("/states", strict_slashes=False)
+@app.route("/states", strict_slashes=False)
 def states():
     states = storage.all("State")
     return render_template("9-states.html", state=states)
 
 
-@flk.route("/states/<id>", strict_slashes=False)
+@app.route("/states/<id>", strict_slashes=False)
 def states_id(id):
     for state in storage.all("State").values():
         if state.id == id:
@@ -20,10 +23,10 @@ def states_id(id):
     return render_template("9-states.html")
 
 
-@flk.teardown_appcontext
+@app.teardown_appcontext
 def teardown(exc):
     storage.close()
 
 
 if __name__ == "__main__":
-    flk.run(host='0.0.0.0', port=5000, debug=False)
+    app.run(host='0.0.0.0', port=5000, debug=False)
