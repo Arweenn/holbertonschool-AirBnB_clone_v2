afed876010ab94f96460a4e01cc881625278d4e8
Arweenn <arwendumont25@outlook.fr>
2024-04-12 15:09:11 +0200

    task 8

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -5,7 +5,7 @@ Script that starts a Flask web app.
 
 from flask import Flask, render_template
 from models import storage
-from models.state import State
+from models.state import State, City
 
 app = Flask(__name__)
 
@@ -17,8 +17,9 @@ def close_stor(exception=None):
 
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
-    states = storage.all(State)
-    return render_template("8-cities_by_states.html", states=states)
+    states = sorted(storage.all(State).values(), key=lambda x: x.name)
+    cities = sorted(storage.all(City).values(), key=lambda x: x.name)
+    return render_template("8-cities_by_states.html", states=states, cities=cities)
 
 
 if __name__ == "__main__":
