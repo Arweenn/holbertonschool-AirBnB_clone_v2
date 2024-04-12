76a6b77e2d7db1b5da743125bfbb3768be43caea
Arweenn <arwendumont25@outlook.fr>
2024-04-12 15:03:25 +0200

    task 8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -9,17 +9,17 @@ from models.state import State
 
 app = Flask(__name__)
 
-
-@app.route('/states_list', strict_slashes=False)
-def list_states():
-    states = storage.all(State).values()
-    return render_template("7-states_list.html", states=states)
-
-
 @app.teardown_appcontext
 def close_stor(exception=None):
+    """Close the SQLAlchemy session"""
     storage.close()
 
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """Get the list of all states"""
+    states = sorted(storage.all(State).values(), key=lambda x: x.name)
+    return render_template('7-states_list.html', states=states)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=False)
